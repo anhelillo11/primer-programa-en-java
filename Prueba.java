5e3f312b7b7df2525c6c1efe321f182d4d7bee5d
Angel Suarez Perez <angelsupe5@gmail.com>
2020-10-02 08:49:18 +0200

    Muestra donde vivo y el sistema operativo que uso

--- Prueba.java
+++ Prueba.java
@@ -3,5 +3,6 @@ public class Prueba {
 	
 		System.out.println("Angel Suarez Perez");
 		System.out.println("Naci en leon a 24 de febrero de 2001");
+		System.out.println("Vivo en leon y utilizo windows 10");
 	}
 }
